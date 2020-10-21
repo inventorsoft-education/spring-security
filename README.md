ed88b4a7613aa700ec094a710c821ba826c8e46b
Igor Potyomkin <igor.potyomkin@inventorsoft.co>
2020-10-21 09:40:27 +0300

    Minor fix;

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 1.You should add security layer (form login authentication) to "send email project". 
    Implement: index.html, login.html, (email payload) page, 
    where user should provide data for email. Unauthenticated users could not send email.
-
-https://github.com/inventorsoft-academy/spring-security.git
--- src/main/java/com/thymeleaf/course/DBInitializer.java
+++ src/main/java/com/thymeleaf/course/DBInitializer.java
@@ -30,21 +30,28 @@ public class DBInitializer {
         user1.setEmail("firstuseremail@test.com");
         user1.setFirstName("first user");
         user1.setLastName("first user");
+
+        /* Use in a case when don't have encoder. */
 //        user1.setPassword("user1Password");
+
         user1.setPassword(encoder.encode("user1Password"));
 
         UserSignUpRequest user2 = new UserSignUpRequest();
         user2.setEmail("seconduseremail@test.com");
         user2.setFirstName("second user");
         user2.setLastName("second user");
-        user2.setPassword("user2Password");
+
+        /* Use in a case when don't have encoder. */
+//        user2.setPassword("user2Password");
         user2.setPassword(encoder.encode("user2Password"));
 
         UserSignUpRequest user3 = new UserSignUpRequest();
         user3.setEmail("thirduseremail@test.com");
         user3.setFirstName("third user");
         user3.setLastName("third user");
-        user3.setPassword("user3Password");
+
+        /* Use in a case when don't have encoder. */
+//        user3.setPassword("user3Password");
         user3.setPassword(encoder.encode("user3Password"));
 
         Arrays.asList(user1, user2, user3)
